2521ad0da560d7e460cecfe639b59a4ce96cc7ed
Rayan <rayan13170@protonmail.com>
2024-05-17 19:27:01 +0200

    successfully added markdown conversion

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket::fs::FileServer;
 use rocket_dyn_templates::Template;
 use serde_json::json;
+use rocket::serde::{Deserialize, json::Json};
 
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Markdown {
+    markdown: String,
+}
 
 #[get("/")]
 fn index() -> Template {
     Template::render("index", json!({}))
 }
 
+#[post("/convert", format = "application/json", data = "<data>")]
+fn convert_md_to_html(data: Json<Markdown>) -> String {
+    let html = comrak::markdown_to_html(&data.markdown, &comrak::ComrakOptions::default());
+    html
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, convert_md_to_html])
         .attach(Template::fairing())
         .mount("/", FileServer::from("static"))
 }
