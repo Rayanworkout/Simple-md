6115d88bc26b6d2c4286a35d7ab2a7cf8d923251
Rayan <rayan13170@protonmail.com>
2024-05-17 22:02:56 +0200

    successfully implemented conversion and display

--- static/js/index.js
+++ static/js/index.js
@@ -2,29 +2,47 @@
 const preview = document.querySelector('#preview');
 const inputField = document.querySelector('#editor');
 
+inputField.addEventListener('input', () => {
+    debounceInput(2000);
+});
 
-let debounceTimer;
-
-const debounceInput = (value, delay) => {
-    clearTimeout(debounceTimer);
-
-    debounceTimer = setTimeout(() => {
-        // Make your POST request here
-        fetch('/convert', {
+const convertMarkdown = async (data) => {
+    try {
+        const response = await fetch('/convert', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ markdown: value })
-        })
-        .then(response => response.text())
-        .then(html => {
-            preview.innerHTML = html;
+            body: JSON.stringify({ markdown: data })
         });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return await response.text();
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    };
+}
+
+const updatePreview = async () => {
+    try {
+
+        const html = await convertMarkdown(inputField.value);
+        preview.innerHTML = html;
+    } catch (error) {
+        console.error('Error updating preview:', error);
+    }
+}
+
+let debounceTimer;
+
+const debounceInput = (delay) => {
+    clearTimeout(debounceTimer);
+
+    debounceTimer = setTimeout(async () => {
+        updatePreview();
+
     }, delay);
 };
-
-inputField.addEventListener('input', () => {
-    const value = inputField.textContent;
-    debounceInput(value, 2500);
-});
