a4ec0ec799fa6961765ac79b7b88fd520efd6c49
rayan <rayan13170@protonmail.com>
2024-05-18 16:53:47 +0200

    successfully converts each cell

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,29 @@
 document.addEventListener('DOMContentLoaded', (_) => {
-    const notebook = document.querySelector('#notebook');
+    const notebook = document.querySelector('.notebook');
 
     notebook.addEventListener('keydown', (event) => {
         if (event.key === 'Enter' && event.ctrlKey) {
             event.preventDefault();
+            htmx.ajax('POST', '/convert', { target: event.target, swap: 'outerHTML', source: event.target })
             createNewCell(notebook);
         }
+    });
+
+    const textarea = document.querySelector('#markdown-content');
+
+    // Dynamically resize textarea
+    textarea.addEventListener('input', function () {
+        textarea.style.height = 'auto';
+        textarea.style.height = textarea.scrollHeight + 'px';
     });
 });
 
 
 function createNewCell(notebook) {
-    const newCell = document.createElement('div');
-    newCell.classList.add('cell');
-    newCell.contentEditable = 'true';
-    notebook.appendChild(newCell);
-    newCell.focus();
+    const newtextArea = document.createElement('textarea');
+    newtextArea.classList.add('cell');
+    newtextArea.name = 'markdown-content';
+    notebook.appendChild(newtextArea);
+    newtextArea.focus();
 }
+
